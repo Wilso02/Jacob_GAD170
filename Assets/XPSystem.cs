ea5866ae4d4f53a61639be5843f45b2272f7e91d
Wilso02 <99461275+Wilso02@users.noreply.github.com>
2022-02-25 16:35:20 +1100

    Assignment Update

--- Assets/XPSystem.cs
+++ Assets/XPSystem.cs
@@ -25,6 +25,8 @@ public class XPSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        InitialStates();
+
         Debug.Log("Welcome to Alien Attack! Press 'Spacebar' to begin!");
         Debug.Log("To view Level press 'T'! ");
         
@@ -33,10 +35,100 @@ public class XPSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Spacebar begins the alien attack
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("You may decide your encounter! Press '1' to fight a xenomorph or '2' to fight a horde of headcrabs!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("Your current level is " + level);
+        }
+
+
+        //Alien Attack
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("You have encountered a Xenomorph, Press 'Q' to attempt to fight!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            // Help needed
+            Debug.Log("Flamethrower Activate!");
+            alienHealth -= alienHealth - attack;
+
+        }
+
+        if (alienHealth <= 0)
+        {
+            Debug.Log("The xenomorph retreated, XP granted!, Press 'Spacebar to return' ");
+            GainExp(25);
+        }
+
+        else
+        {
+            Debug.Log("Xenomorph at HP: " + alienHealth);
+        }
+
+        // Headcrab Horde Encounter
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("You have encountered a Horde of Headcrabs, Press 'W' to attempt to fight!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("Throwing a grenade!");
+            headcrabHealth -= headcrabHealth - attack;
+        }
+
+        if (headcrabHealth <= 0)
+        {
+            Debug.Log("They're all just corpses now! XP Granted, Press 'Spacebar to return' ");
+            GainExp(30);
+            Debug.Log("What's that rumbling? Press '3' to investigate. ");
+        }
+        else
+        {
+            Debug.Log(" Keep throwing soilder! There's only a couple left!" + headcrabHealth);
+        }
+
+
+        //Boss Fight
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("Holy Moses! Thats the Queen Xenomorph! Press 'E' to fight!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("You shoot a rocket, at the Queen!");
+            alienQueen -= alienQueen - attack;
+
+        }
+
+        if (alienQueen <= 0)
+        {
+            Debug.Log("The Queen has fallen! Well Done Soilder! Proceed to Exfil! Press 'Z' to Exfil");
+
+        }
+        else
+        {
+            Debug.Log("The Queen still stands! Reload! Reload!" + alienQueen);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Congrats Soilder on making it outta their alive! However reports state theres another attack! Your level was " + level);
+        }
+        // Check with teacher on why it constantly runs
+
     }
 
-    public void InitalStates()
+    public void InitialStates()
     {
         //Enemies Health
         alienHealth = 50;
@@ -58,100 +150,9 @@ public class XPSystem : MonoBehaviour
 
     }
 
-    public void Interaction()
-    {
-
-        //this is called by button presses, should grant a diff amount of exp
-        // Spacebar begins the alien attack
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("You may decide your encounter! Press '1' to fight a xenomorph or '2' to fight a horde of headcrabs!");
-        }
-
-        if(Input.GetKeyDown(KeyCode.T))
-        {
-            Debug.Log("Your current level is " + level);
-        }
-       
-
-        //Alien Attack
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            Debug.Log("You have encountered a Xenomorph, Press 'Q' to attempt to fight!");
-        }
-
-        if(Input.GetKeyDown(KeyCode.Q))
-        {
-            // Help needed
-            Debug.Log("Flamethrower Activate!");
-            alienHealth = alienHealth - attack;
-
-        }
-
-        if (alienHealth <= 0)
-        {
-            Debug.Log("The xenomorph retreated, XP granted!, Press 'Spacebar to return' ");
-            GainExp(25);
-        }
-
-        else
-        {
-            Debug.Log("Xenomorph at HP: " + alienHealth);
-        }
-
-        // Headcrab Horde Encounter
-        if(Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            Debug.Log("You have encountered a Horde of Headcrabs, Press 'W' to attempt to fight!");
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Debug.Log("Throwing a grenade!");
-            headcrabHealth = headcrabHealth - attack;
-        }
-
-        if(headcrabHealth <=0)
-        {
-            Debug.Log("They're all just corpses now! XP Granted, Press 'Spacebar to return' ");
-            GainExp(30);
-            Debug.Log("What's that rumbling? Press '3' to investigate. ");
-        }
-        else
-        {
-            Debug.Log(" Keep throwing soilder! There's only a couple left!" + headcrabHealth);
-        }
-
-
-        //Boss Fight
-        if(Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            Debug.Log("Holy Moses! Thats the Queen Xenomorph! Press 'E' to fight!");
-        }
-
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            Debug.Log("You shoot a rocket, at the Queen!");
-            alienQueen = alienQueen - attack;
-
-        }
-
-        if(alienQueen <=0)
-        {
-            Debug.Log("The Queen has fallen! Well Done Soilder! Proceed to Exfil! Press 'Z' to Exfil");
-
-        }
-        else
-        {
-            Debug.Log("The Queen still stands! Reload! Reload!" + alienQueen);
-        }
-
-        if(Input.GetKeyDown(KeyCode.Z))
-        {
-            Debug.Log("Congrats Soilder on making it outta their alive! However reports state theres another attack! Your level was " + level);
-        }
-            // Check with teacher on why it constantly runs
-    }
+    
+   
+    
 
     public void GainExp(float gain)
     {
